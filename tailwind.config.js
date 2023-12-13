a903ab29fddcbd898299d44f37cfe4f5d079f7d0
snigdhabhoir <153580772+snigdhabhoir@users.noreply.github.com>
2023-12-13 21:38:36 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,,jsx,}",
+  ],
+  theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    extend: {
+      colors: {
+        "light-blue": "#227fb4",
+        "light-Gray": "#9ca3af",
+        "dark-Gray": " #202020"
+      }
+    },
+  },
+  plugins: [],
+}
+
